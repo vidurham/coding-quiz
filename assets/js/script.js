30159c408580fe64793aa718f66b771b5b8a3e61
Vinny D <vdurham@prideseals.com>
2022-02-16 11:41:05 -0500

    start button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-var startButton = document.querySelector(".start-btn button") 
+var startBtn = document.querySelector(".start-btn button");
+var timerEl = document.querySelector("#seconds");
+var homeEl = document.querySelector("#home");
+var quizEl = document.querySelector("#quiz");
 
 
 var questions = [
     {}
 ]
+
+var startTimer = function () {
+    homeEl.classList.add("gone");
+    quizEl.classList.remove("gone");
+}
+
+startBtn.addEventListener("click", startTimer);

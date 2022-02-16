b8b534263f53c397a97856534e8373e08db0d86b
Vinny D <vdurham@prideseals.com>
2022-02-16 12:24:14 -0500

    added first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,53 @@ var startBtn = document.querySelector(".start-btn button");
 var timerEl = document.querySelector("#seconds");
 var homeEl = document.querySelector("#home");
 var quizEl = document.querySelector("#quiz");
-var secondsLeft = 75
+var questionEl = document.querySelector(".question");
+var answersEl = document.querySelector(".answers");
+var secondsLeft = 75;
+var questionNumber = -1;
 
 
 var questions = [
-    {}
+    {question: "What is my Name?",
+    answer : "Vincent",
+    options : ["Richard", "Vincent", "Doug", "Bo"]
+}
 ]
 
 var startTimer = function () {
     homeEl.classList.add("gone");
     quizEl.classList.remove("gone");
 
-    setTimer()
+    setTimer();
+    makeQuiz();
 }
 
 function setTimer () {
     var count = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = "Time Left: " + secondsLeft;
-    }, 1000)
+
+        if (secondsLeft === 0 || questionNumber === questions.length){
+            clearInterval(count);
+
+        }
+    }, 1000);
 }
+
+function makeQuiz () {
+    questionNumber++;
+    answer = questions[questionNumber].answer;
+
+    questionEl.textContent = questions[questionNumber].question;
+
+    var options = questions[questionNumber].options;
+
+    for (var i = 0; i < options.length; i++) {
+        var nextChoice = document.createElement("button");
+        nextChoice.textContent = options[i];
+        answerBtn = answersEl.appendChild(nextChoice)
+    }
+}
+
 
 startBtn.addEventListener("click", startTimer);

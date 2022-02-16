c00eee87aeee48afb2440b7083c808d49f5de682
Vinny D <vdurham@prideseals.com>
2022-02-16 11:54:56 -0500

    timer is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var startBtn = document.querySelector(".start-btn button");
 var timerEl = document.querySelector("#seconds");
 var homeEl = document.querySelector("#home");
 var quizEl = document.querySelector("#quiz");
+var secondsLeft = 75
 
 
 var questions = [
@@ -11,6 +12,15 @@ var questions = [
 var startTimer = function () {
     homeEl.classList.add("gone");
     quizEl.classList.remove("gone");
+
+    setTimer()
+}
+
+function setTimer () {
+    var count = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = "Time Left: " + secondsLeft;
+    }, 1000)
 }
 
 startBtn.addEventListener("click", startTimer);
